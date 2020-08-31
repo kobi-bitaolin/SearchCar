9a1ec3589582794d2bdf7e701930fb6cfb422854
kobi bitaolin <amk999@gmail.com>
2020-08-31 19:03:59 +0300

    herokucarsapp

--- server/server.js
+++ server/server.js
@@ -27,12 +27,8 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-    const root = path.join(__dirname, "..", "client", "build");
-    app.use(express.static(root));
-    app.get("*", (req, res) => {
-      res.sendFile("index.html", { root });
-    });
-  }
+    app.use(express.static('client/build'))
+}
 
 
 app.listen(PORT, () => {
