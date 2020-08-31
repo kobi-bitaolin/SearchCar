e5eb26354077b4da014f4e5d9bc8e4991481a73a
kobi bitaolin <amk999@gmail.com>
2020-08-31 20:18:39 +0300

    uptoheroku

--- server/server.js
+++ server/server.js
@@ -5,17 +5,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const cookieParser = require('cookie-parser');
 const carApi = require("./carapi.json");
-// const path = require('path');
+const path = require('path');
 dotenv.config();
 const db = 'mongodb+srv://' + process.env.MONGO_ATLAS + '@cluster0.pwfuj.mongodb.net/<dbname>?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 4000;
 
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connection
-    .once('open', () => console.log('mongodb Connected'))
-    .on('error', (error) => {
-        console.log('mongodb erorr', error);
-    })
+.once('open', () => console.log('mongodb Connected'))
+.on('error', (error) => {
+    console.log('mongodb erorr', error);
+})
 
 app.get('/cars', (req, res) => {
     return res.send(carApi)
@@ -27,11 +26,16 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static('client/build'))
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
 }
 
+app.use('/users', require('./routes/userRouter'));
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`server is listen on ${PORT}`);
 });
-app.use('/users', require('./routes/userRouter'));
